3110b9d661f105c118771c251ea836da8cf5b428
Luke Watts <luke@luke-watts.com>
2015-04-01 12:04:51 +0100

    Adding conditional to remove Contact Form 7 from admin if it's installed.

--- affinity4-api.php
+++ affinity4-api.php
@@ -21,13 +21,22 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+if (! defined( 'WPINC' )) {
 	die;
 }
 
+// This is to hide Contact Form 7 Admin Page from all bit Admin role
+if (is_file(plugin_dir_path(__FILE__) . '../contact-form-7/wp-contact-form-7.php')) {
+	define('WPCF7_ADMIN_READ_CAPABILITY', 'manage_options');
+	define('WPCF7_ADMIN_READ_WRITE_CAPABILITY', 'manage_options');
+}
+
+// Set path constants
 define('API_BASE', plugin_dir_path(__FILE__));
 define('API_LIBS', API_BASE . 'libs/');
 
+// require Class
+// TODO: Use PSR-4 Autoloader with SPL Autoloader fallback
 require_once(API_LIBS . 'Asset.php');
 require_once(API_LIBS . 'Shortcode.php');
 require_once(API_LIBS . 'QuickTag.php');
